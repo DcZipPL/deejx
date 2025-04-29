b36f585129a90bc0899f2d0b89f4bfe7818527fb
Kordian Kisieliński <dczip89@gmail.com>
2025-04-29 16:14:15 +0200

    Comment out midi for now

--- driver/src/midi/mod.rs
+++ driver/src/midi/mod.rs
@@ -1,6 +1,6 @@
 // todo: There will be platform code required. midir supports unix, but windows support was removed. For windows `rtmidi` bindings will be used or loopMIDI virtual device
 
-use midir::{MidiOutput, MidiOutputConnection};
+/*use midir::{MidiOutput, MidiOutputConnection};
 fn debug_midi(value: u8, cc_number: u8) -> anyhow::Result<()> {
     let midi_out = MidiOutput::new("Deejx Midi Output")?;
     let out_ports = midi_out.ports();
@@ -15,4 +15,4 @@ fn debug_midi(value: u8, cc_number: u8) -> anyhow::Result<()> {
     let mut conn_out = midi_out.connect(port, "loopMIDI Port").unwrap();
     conn_out.send(&[0xB0, 1, 64])?; // send a CC message
     Ok(())
-}
+}*/
